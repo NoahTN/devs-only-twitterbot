5c71b8e8b9710f64ae524e8208f9f9fd2187a262
NoahTN <ntolentino-nguyen@csumb.edu>
2019-11-02 15:52:20 -0700

    Update to return string

--- air.py
+++ air.py
@@ -2,30 +2,29 @@
 from waqi_python import client as base
 
 class WeatherAPI:
-    def __init__(self, city):
-        client = base.WaqiClient()
-        my_station = client.get_local_station()
-        my_station.city.name = city
-        value = my_station.aqi
-
-        print('Air Quality Index is: ')
-        print(my_station.aqi)
-
-        print('Your Air Quality Is: ')
+    def __init__(self):
+        self.client = base.WaqiClient()
+        self.my_station = self.client.get_local_station()
+  
+    def get_aqi(self, city):
+        self.my_station.city.name = city
+        value = self.my_station.aqi
+        quality = ""
 
         if value < 50: #0-50
-            print ('Good')
+            quality = 'Good'
         elif 50 < value < 101: #51-100
-            print ('Moderate')
+            quality = 'Moderate'
         elif 100 < value < 151: #101-150
-            print ('Unhealthy for sensitive people')
+            quality = 'Unhealthy for sensitive people'
         elif 150 < value < 201: #151-200
-            print ('Unhealthy')
+            quality = 'Unhealthy'
         elif 200 < value < 301: #201-300
-            print ('Very Unhealthy')
-        elif value > 300: #301+
-            print ('Hazardous')
+            quality = 'Very Unhealthy'
+        else:
+            quality = 'Hazardous'
 
+        return f'Your Air Quality is: {quality}'
         
 wapi = WeatherAPI()
-
+print(wapi.get_aqi("Monterey"))
