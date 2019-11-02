fef0860df595f34c8492caa158be199648e42d19
ElijahHallera <57295509+ElijahHallera@users.noreply.github.com>
2019-11-02 14:54:35 -0700

    Update air.py
    Updated air to include text

--- air.py
+++ air.py
@@ -1,11 +1,31 @@
 
+from waqi_python import client as base
+
 class WeatherAPI:
-    def __init__(self):
-        from waqi_python import client as base
+    def __init__(self, city):
         client = base.WaqiClient()
         my_station = client.get_local_station()
-        my_station.city.name = 'San Francisco-Arkansas Street, San Francisco, California'
+        my_station.city.name = city
+        value = my_station.aqi
+
+        print('Air Quality Index is: ')
         print(my_station.aqi)
+
+        print('Your Air Quality Is: ')
+        if value < 50:
+            print ('Good')
+        elif value > 50 && value < 101:
+            print ('Moderate')
+        elif value > 100 && value < 151:
+            print ('Unhealthy for sensitive people')
+        elif value > 150 && value < 201
+            print ('Unhealthy')
+        elif value > 200 && value < 301:
+            print ('Very Unhealthy')
+        elif value > 300:
+            print ('Hazardous')
+
+        
 
 
 wapi = WeatherAPI()
