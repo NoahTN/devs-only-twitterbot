6b9804d0d3cbcac18852177cfb7cd8e5a7fd92ac
BuildTools <unconfigured@null.spigotmc.org>
2019-11-02 11:08:48 -0700

    added keys to post to twitter

--- tweetbot.py
+++ tweetbot.py
@@ -2,10 +2,10 @@
 import tweepy 
   
 # personal details 
-consumer_key ="xxxxxxxxxxxxxxxx"
-consumer_secret ="xxxxxxxxxxxxxxxx"
-access_token ="xxxxxxxxxxxxxxxx"
-access_token_secret ="xxxxxxxxxxxxxxxx"
+consumer_key ="ru5kZBYn4bBnO1hMGYRJKsr11"
+consumer_secret ="UjZ6G7PXm2GPLcjNzqCClwjlhMu1mKsA72sHmDaC0wcDF2UOkI"
+access_token ="1190661254974590976-OAEU10a7qpNr67ysRWqj2UdvGEqxaA"
+access_token_secret ="R47JeIAgB6xyuwowsupGjMHZ1P6KacQMA7gCo0owVRjS1"
   
 # authentication of consumer key and secret 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
@@ -15,4 +15,4 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth) 
   
 # update the status 
-api.update_status(status ="Hello Everyone !")
+api.update_status(status ="Massive Chunglers")
