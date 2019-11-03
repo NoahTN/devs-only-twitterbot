de05888c2f2d8162d1dff0789842c17b97f70908
NoahTN <ntolentino-nguyen@csumb.edu>
2019-11-02 17:40:31 -0700

    Update to use expanded list

--- outages.py
+++ outages.py
@@ -1,27 +1,18 @@
 import requests
 from datetime import datetime
+from pytz import timezone
+import pytz
 
 class OutageAPI:
-    def __init__(self):
-        self.region_codes = {}
-        # should performed only once
-        regions = requests.get('https://pge-outages.simonwillison.net/pge-outages.json?sql=select+*+from+regionName').json()
-        for region in regions["rows"]:
-            self.region_codes[region[1]] = region[0]
 
     def get_data(self, city):
-        if city not in self.region_codes:
-            return 'Untracked city.'
-        
-        region_code = self.region_codes[city]
-
-        json_data = requests.get(f'https://pge-outages.simonwillison.net/pge-outages.json?sql=select+*+from+outages+where+%22regionName%22+%3D+%3Ap0+order+by+outageStartTime+desc+limit+1&p0={region_code}').json()
+        json_data = requests.get(f'https://pge-outages.simonwillison.net/pge-outages.json?sql=select+%2A+from+outages_expanded+where+%22min_estCustAffected%22+%3E+%3Ap0+and+%22region%22+%3D+%3Ap1+order+by+earliest+desc+limit+1&p0=9&p1={city}').json()
 
         if len(json_data['rows']) == 1:
             date_format='%A, %B %d at %I:%M %p'
             outage_date = datetime.fromtimestamp(json_data['rows'][0][1])
-            print(outage_date)
             if (datetime.now() - outage_date).days < 30:
+                outage_date = outage_date.astimezone(timezone('US/Pacific'))
                 return f'Recent outage on {outage_date.strftime(date_format)} UTC'
                 
         return 'No recent outages.'
