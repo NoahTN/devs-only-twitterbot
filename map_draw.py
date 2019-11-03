e610fe9021ab0abf3c0832b6402bbbbb0a63ae76
NoahTN <ntolentino-nguyen@csumb.edu>
2019-11-02 19:52:36 -0700

    Delete image saving

--- map_draw.py
+++ map_draw.py
@@ -6,7 +6,6 @@ class MapDraw:
     def get_image(self, coords):
         response = requests.get('https://www.mapquestapi.com/staticmap/v5/map?boundingBox=36.74,-121.99,36.5,-121.59&zoom=11&traffic=flow|cons|inc&size=700,500@2x&key=MfFt1rJi4T1sJLHkfIaITmfEzMdO57HM')
         img = Image.open(BytesIO(response.content))
-        img.save("test.jpg")
         return img
 
 map_draw = MapDraw()
