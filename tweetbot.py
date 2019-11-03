03f4d9d879d4902c1fecc3607d455d21e77fe6e0
BuildTools <mwhaley@csumb.edu>
2019-11-02 19:36:53 -0700

    fixed traffic.py

--- tweetbot.py
+++ tweetbot.py
@@ -65,27 +65,6 @@ outages = OutageAPI()
 a = outages.get_data('Monterey')
 
 
-class TrafficAPI:
-    # get multiple
-    def get_data(self):
-        # makes request
-        json_data = requests.get('http://www.mapquestapi.com/traffic/v2/incidents?key=MfFt1rJi4T1sJLHkfIaITmfEzMdO57HM&boundingBox=39.95,-105.25,39.52,-104.71&filters=construction,incidents').json()
-        incidents = json_data["incidents"]
-        shortDesc = []
-        for i in incidents:
-            #print(i)
-            if i["type"]  > 2 :
-                shortDesc.append(i["shortDesc"])
-                #print(i["shortDesc"])
-        #print(json_data)
-        if len(shortDesc) == 0:
-            masterString = "The roads look clear"
-        else:
-            for x in range(2):
-                masterString += shortDesc[x]
-                masterString += "\n"
-        return masterString
-        
 
 # test
 traffic = TrafficAPI()
